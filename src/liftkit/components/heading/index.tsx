abe32748e8edd37ef9d5df699936893c482a2142
radodd <ethanf.flores@gmail.com>
2025-05-01 15:06:18 -0400

    feat/heading component

--- src/liftkit/components/heading/index.tsx
+++ src/liftkit/components/heading/index.tsx
@@ -6,24 +6,28 @@ type LkHeadingTag = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
 interface LkHeadingProps extends React.HTMLAttributes<HTMLHeadingElement> {
   tag?: LkHeadingTag;
   fontClass?: string; // Should be LkFontClass in production
-  content?: string;
+  children?: React.ReactNode;
+  fontColor?: string
+  // content?: string;
 }
 
 export default function Heading({
   tag = "h2",
   fontClass = "display2-bold",
-  content = "Heading",
-  ...rest
+fontColor,
+  // content = "Heading",
+  children,
+  ...restProps
 }: LkHeadingProps) {
   const headingAttrs = useMemo(
-    () => propsToDataAttrs(rest, "lk-heading"),
-    [rest],
+    () => propsToDataAttrs(restProps, "heading"),
+    [restProps],
   );
   const Tag = tag;
 
   return (
-    <Tag className={`lk-font-${fontClass}`} {...headingAttrs}>
-      {content}
+    <Tag lk-component="heading" className={`${fontClass} color-${fontColor}`} {...headingAttrs}>
+      {children}
     </Tag>
   );
 }
