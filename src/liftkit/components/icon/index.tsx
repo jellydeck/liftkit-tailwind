5faa6c275c27b3bfe5fb5436869e37caff6fd45f
radodd <ethanf.flores@gmail.com>
2025-05-02 10:06:43 -0400

    fix/fontClass font-size logic

--- src/liftkit/components/icon/index.tsx
+++ src/liftkit/components/icon/index.tsx
@@ -2,6 +2,7 @@
 
 import { DynamicIcon } from "lucide-react/dynamic";
 import type { IconName } from "lucide-react/dynamic";
+import { useEffect, useRef, useState } from "react";
 
 interface LkIconProps extends React.HTMLAttributes<HTMLElement> {
   name?: IconName;
@@ -16,13 +17,33 @@ export default function Icon({
   fontClass,
   color,
   strokeWidth = 2,
+  ...restProps
 }: LkIconProps) {
+  const ref = useRef<HTMLDivElement>(null);
+  const [size, setSize] = useState<number | undefined>(undefined)
+
+  useEffect(() => {
+    if (ref.current) {
+      const computedStyle = getComputedStyle(ref.current)
+      console.log("Computed Style:", computedStyle) 
+      const fontSize = parseFloat(computedStyle.fontSize);
+      console.log("fontSize:", fontSize)
+      if (!isNaN(fontSize)) {
+        setSize(fontSize)
+      }
+    }
+  }, [fontClass])
+console.log("Size:", size)
+
   return (
-    <div lk-component="icon">
+    <div lk-component="icon"
+    {...restProps}
+    > <span ref={ref} className={fontClass} style={{ position: "absolute", visibility: "hidden", height: 0, overflow: "hidden" }}>
+    A
+  </span>
       <DynamicIcon
         name={name}
-        // Unclear what fontClass purpose is here
-        className={fontClass}
+        size={size}
         color={`var(--lk-${color})`}
         strokeWidth={strokeWidth}
       />
