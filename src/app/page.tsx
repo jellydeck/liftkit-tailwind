d8646f4230346da55cddbbcda35cb8f086c44da9
radodd <ethanf.flores@gmail.com>
2025-05-05 13:11:06 -0400

    fix/icon component - support dynamic font-size sizing from fontClass prop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Badge from "@/liftkit/components/badge";
+import Icon from "@/liftkit/components/icon";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <Badge></Badge>
+      <Icon name="airplay" color="primary" fontClass="title2" />
     </div>
   );
 }
--- src/liftkit/components/icon/index.tsx
+++ src/liftkit/components/icon/index.tsx
@@ -1,8 +1,5 @@
-"use client";
-
 import { DynamicIcon } from "lucide-react/dynamic";
 import type { IconName } from "lucide-react/dynamic";
-import { useEffect, useRef, useState } from "react";
 
 interface LkIconProps extends React.HTMLAttributes<HTMLElement> {
   name?: IconName;
@@ -19,31 +16,18 @@ export default function Icon({
   strokeWidth = 2,
   ...restProps
 }: LkIconProps) {
-  const ref = useRef<HTMLDivElement>(null);
-  const [size, setSize] = useState<number | undefined>(undefined)
 
-  useEffect(() => {
-    if (ref.current) {
-      const computedStyle = getComputedStyle(ref.current)
-      console.log("Computed Style:", computedStyle) 
-      const fontSize = parseFloat(computedStyle.fontSize);
-      console.log("fontSize:", fontSize)
-      if (!isNaN(fontSize)) {
-        setSize(fontSize)
-      }
-    }
-  }, [fontClass])
-console.log("Size:", size)
+
+
 
   return (
     <div lk-component="icon"
-    {...restProps}
-    > <span ref={ref} className={fontClass} style={{ position: "absolute", visibility: "hidden", height: 0, overflow: "hidden" }}>
-    A
-  </span>
+    {...restProps} lk-icon-font-class={fontClass}>
+ 
       <DynamicIcon
         name={name}
-        size={size}
+        width="1em"
+        height="1em"
         color={`var(--lk-${color})`}
         strokeWidth={strokeWidth}
       />
