52f7f7f19c3f415478641930c79c303ee0ef5fb1
radodd <ethanf.flores@gmail.com>
2025-05-05 11:53:13 -0400

    fix/tabs component - rebase with staging

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import styles from "./page.module.css";
 import Section from "@/liftkit/components/section";
 import Text from "@/liftkit/components/text";
@@ -56,6 +57,17 @@ export default function Home() {
 
   return (
     <div className={styles.page}>
+      <Tabs
+        tabLinks={tabLabels}
+        activeTab={activeTab}
+        setActiveTab={setActiveTab}
+      >
+        {tabLabels.map((label, index) => (
+          <TabContent key={index}>
+            <p>This is the content for {label}</p>
+          </TabContent>
+        ))}
+      </Tabs>
       <Grid columns={2} gap="md">
         <div style={{ background: "red" }}>Item 1</div>
         <div style={{ background: "blue" }}>Item 2</div>
@@ -420,6 +432,18 @@ export default function Home() {
       <Text fontClass="display1" tag="footer" color="primary">
         Hello World
       </Text>
+      <Paragraph fontClass="title1">
+        ancient times, cats were not merely companions—they were revered as
+        divine beings. Cultures like ancient Egypt honored cats as sacred
+        creatures, embodying grace, mystery, and spiritual power. The goddess
+        Bastet, depicted with the head of a lioness or domestic cat, symbolized
+        protection, fertility, and the nurturing aspects of home life. Even
+        beyond Egypt, the enigmatic nature of cats—their watchful eyes, silent
+        movements, and uncanny independence—has inspired a timeless belief that
+        they walk between worlds. To this day, many still joke (or suspect) that
+        cats aren’t just pets, but deities in disguise, quietly ruling their
+        human households with regal indifference.
+      </Paragraph>
     </div>
   );
 }
